5878695c9fe5fb0824a13f72e9f5c00e94ae71a9
Ak160505 <107071963+Ak160505@users.noreply.github.com>
2023-10-26 18:08:38 +0530

    first commit

---
+++ src/Components/Contact.js
@@ -0,0 +1,10 @@
+function Contact(){
+    return(
+        <div>
+            <div id='contactdet' className='container-fluid text-start'>Mobile Number:<span> 9533477786</span></div>
+            <div id='contactdet' className='container-fluid text-start'>Email ID:<span> abdulkareem16505@gmail.com</span></div>
+            <div id='contactdet' className='container-fluid text-start'>Address: <span>NVR Street, Madanapalli, Annamayya district, Andhra Pradesh, India 517325</span></div>
+        </div>
+    )
+}
+export default Contact;
---
+++ src/Components/EducationalInfo.js
@@ -0,0 +1,51 @@
+function EducationalInfo() {
+    return (
+        <div>
+            <div class="card mb-3" id="clgContainer">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="https://images.collegedunia.com/public/college_data/images/logos/1615969516Screenshot20210317134244.png" class="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">VIT-AP University</h5>
+                            <p class="card-text">Bachelor of Technology in Computer Science and Engineering with a Specialization in Artificial Intelligence and Machine Learning</p>
+                            <p class="card-text">CGPA : 8.87</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img id="interclg" src="https://sulcdn.azureedge.net/biz-live/img/438724-2545735-28022017132214.jpeg" class="img-fluid rounded-start" alt="..." />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Narayana Junior College</h5>
+                            <p class="card-text">Intermediate in Maths Physincs and Chemistry (Mpc)</p>
+                            <p class="card-text">JEE Mains percentile : 94.2%</p>
+                            <p class="card-text">AP Eamcet rank: 3706</p>
+                            <p class="card-text">Marks : 978</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="https://th.bing.com/th/id/OIP.ypzO0d0ugNi4f1KYHH2gsAAAAA?pid=ImgDet&w=350&h=350&rs=1" alt="..." />
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body" id="schoolContent">
+                            <h5 class="card-title">Narayana English Medium High School</h5>
+                            <p class="card-text">Class 10</p>
+                            <p class="card-text">GPA :10.0</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default EducationalInfo;
